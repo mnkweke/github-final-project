75b98ceb819a3b7f66b6cfde10f6bcfbfc08eddc
mnkweke <38846108+mnkweke@users.noreply.github.com>
2025-05-16 13:43:36 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,35 +2,6 @@ Task 6: Host the script file
 Create a new file named simple-interest.sh in the root directory of the repository.
 
 Add the following code in the new file:
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
    #!/bin/bash
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
